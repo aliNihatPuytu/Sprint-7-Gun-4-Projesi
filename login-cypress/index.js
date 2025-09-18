80625cb3a950404093989443697d9637ea48934b
Ali Nihat Puytu <alinihatpuytu@gmail.com>
2025-09-18 13:50:00 +0300

    Commit 1.1

---
+++ login-cypress/index.js
@@ -0,0 +1,10 @@
+function cypressTestProjesi() {
+  const checklist = {
+    
+    project_url: 'https://github.com/aliNihatPuytu/Sprint-7-Gun-4-Projesi',
+  };
+
+  return checklist;
+}
+
+module.exports = cypressTestProjesi;
---
+++ login-cypress/src/errorMessages.js
@@ -0,0 +1,6 @@
+export const errorMessages = {
+  emailInvalid: 'Lütfen geçerli bir email adresi girin.',
+  passwordInvalid:
+    'Şifreniz en az 8 karakter olmalı; bir büyük, bir küçük harf, bir rakam ve bir özel karakter içermelidir.',
+  termsRequired: 'Devam etmek için şartları kabul etmelisiniz.',
+};
